1bb0b82b93158309cafbee5ab1d8519d64f88abf
SnippyCola <sandya.suresh@gmail.com>
2024-10-17 11:42:09 -0400

    removed debug statements

--- main.js
+++ main.js
@@ -7,26 +7,20 @@ function retrieveData(location){
     xhr.send();
     xhr.onload = function() {
         var response = xhr.response;
-        var dispA = document.getElementById("display_data");
-        //dispA.innerHTML = dispA.innerHTML + " 1";
         if (xhr.status != 200) { 
             dispA.innerHTML = "XMLHttpRequest error: "+ xhr.status;
             return;
         }
-        //dispA.innerHTML = dispA.innerHTML + " 2";
         if (!response) {
             dispA.innerHTML = "Empty response";
             return;
         }
-        //dispA.innerHTML = dispA.innerHTML + " 4";
         if (response.errorCode>0) {
-            dispA.innerHTML = "Error detected. errorCode="+response.errorCode+", message="+response.message;
+            console.log("Error detected. errorCode="+response.errorCode+", message="+response.message);
             return;
         }
 
-        //dispA.innerHTML = dispA.innerHTML + " 4";
         var rows = document.getElementById("disp_table").rows;
-        dispA.innerHTML = dispA.innerHTML + rows.length;
         for(let i = 1; i < rows.length; i++){
             let loc = rows.item(i).children.item(0).innerHTML;
             if(response.resolvedAddress.localeCompare(loc) == 0){
@@ -35,14 +29,10 @@ function retrieveData(location){
         }
         addRow(response);
     };
-    // return xhr.response;
 };
 
 function displayData(){
-    //addRow();
     retrieveData(document.getElementById("req_loc").value);
-    //var dispA = document.getElementById("display_data");
-    //dispA.innerHTML = JSON.stringify(data);
 }
 
 function addRow(response){
