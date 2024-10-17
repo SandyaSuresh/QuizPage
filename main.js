c183a9b890749c0d264368bb103d2def2ac9f030
SnippyCola <sandya.suresh@gmail.com>
2024-10-17 10:40:43 -0400

    functions

--- main.js
+++ main.js
@@ -6,30 +6,58 @@ function retrieveData(location){
     xhr.open("GET", "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + location + "?key=" + KEY);
     xhr.send();
     xhr.onload = function() {
+        var response = xhr.response;
+        var dispA = document.getElementById("display_data");
+        //dispA.innerHTML = dispA.innerHTML + " 1";
         if (xhr.status != 200) { 
             dispA.innerHTML = "XMLHttpRequest error: "+ xhr.status;
             return;
         }
+        //dispA.innerHTML = dispA.innerHTML + " 2";
         if (!response) {
             dispA.innerHTML = "Empty response";
             return;
         }
-        
+        //dispA.innerHTML = dispA.innerHTML + " 4";
         if (response.errorCode>0) {
             dispA.innerHTML = "Error detected. errorCode="+response.errorCode+", message="+response.message;
             return;
         }
-        var dispA = document.getElementById("display_data");
-        dispA.innerHTML = JSON.stringify(xhr.response);
-        return JSON.stringify(xhr.response);
+
+        //dispA.innerHTML = dispA.innerHTML + " 4";
+        addRow(response);
     };
-    // var dispA = document.getElementById("display_data");
-    // dispA.innerHTML = JSON.stringify(xhr.response);
     // return xhr.response;
 };
 
 function displayData(){
-    var data = retrieveData(document.getElementById("req_loc").value);
+    //addRow();
+    retrieveData(document.getElementById("req_loc").value);
     //var dispA = document.getElementById("display_data");
     //dispA.innerHTML = JSON.stringify(data);
+}
+
+function addRow(response){
+    var table = document.getElementById("disp_table");
+    var newRow = table.insertRow();
+
+    let locCell = newRow.insertCell();
+    let locText = document.createTextNode(response.resolvedAddress);
+    locCell.appendChild(locText);
+    
+    let maxCell = newRow.insertCell();
+    let maxText = document.createTextNode(response.days[0].tempmax);
+    maxCell.appendChild(maxText);
+    
+    let minCell = newRow.insertCell();
+    let minText = document.createTextNode(response.days[0].tempmin);
+    minCell.appendChild(minText);
+
+    let humCell = newRow.insertCell();
+    let humText = document.createTextNode(response.days[0].humidity + "%");
+    humCell.appendChild(humText);
+    
+    let condCell = newRow.insertCell();
+    let condText = document.createTextNode(response.days[0].conditions);
+    condCell.appendChild(condText);
 }
